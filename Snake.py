f46a78782d0c4abeb1887ea07da7567c676e631c
camcamcamcamcam <eboysix@hotmail.co.nz>
2025-05-11 21:23:08 +1200

    Ignore this idea

---
+++ Snake.py
@@ -0,0 +1,59 @@
+from enum import Enum
+
+class Direction(Enum):
+    UP = 0
+    RIGHT = 1
+    DOWN = 2
+    LEFT = 3
+
+class Part(Enum):
+    HEAD = 0
+    LUMP = 1
+    BODY = 2
+    TAIL = 3
+
+class Snake:
+
+    def __init__(self, body=None, facing=Direction.RIGHT):
+        self.facing = facing
+        if body is None:
+            self.body = [[0,0,Part.HEAD]]
+        else:
+            self.body = body
+
+    def turn_left(self):
+        self.facing = Direction((self.facing.value + 3) % 4)
+
+    def turn_right(self):
+        self.facing = Direction((self.facing.value + 1) % 4)
+
+    def delta(self):
+        match self.facing.value:
+            case 0:
+                return 0, -1
+            case 1:
+                return 1, 0
+            case 2:
+                return 0, 1
+            case 3:
+                return -1, 0
+        return 0, 0
+
+    def tick(self):
+        # move snake
+        delta = self.delta()
+        self.body.append([self.body[-1][0] + delta[0], self.body[-1][1] + delta[1], Part.HEAD])
+
+        # make sure its body parts are all correct
+        if len(self.body) < 1 or not self.body[0][2] is Part.LUMP:
+            self.body.pop(0)
+        self.body[0][2] = Part.TAIL
+
+        for i in range(len(self.body) - 1):
+
+            if self.body[i][2] is Part.HEAD:
+                self.body[i][2] = Part.BODY
+            if self.body[i + 1][2] is Part.LUMP:
+                self.body[i + 1][2] = Part.BODY
+                self.body[i][2] = Part.LUMP
+
