cbaecf85f8a77fac4f6f71101dd51469512edaf4
camcamcamcamcam <eboysix@hotmail.co.nz>
2025-05-22 13:23:35 +1200

    Snake is now controllable with WASD

--- Main.py
+++ Main.py
@@ -1,9 +1,7 @@
-import curses
-
 import unicurses
 
 from Board import Board
-from Snake import Snake, Part
+from Snake import Snake, Part, Direction
 
 from curses import wrapper
 import time
@@ -13,13 +11,17 @@ def main(stdscr):
 
     board = Board()
     board.add_snake(Snake([[0, 0, Part.TAIL], [0, 1, Part.BODY], [1, 1, Part.LUMP], [2, 1, Part.HEAD]]))
+    key = 0
+    while key != ord('Q'):
+        direction = "asdw".find(chr(key))
+        if 0 <= direction <= 3:
+            for snake in board.snakes:
+                snake.facing = Direction(direction)
 
-    for i in range(10):
-        time.sleep(0.1)
         stdscr.clear()
-        stdscr.addstr(str(board))
         board.tick()
+        stdscr.addstr(str(board))
         stdscr.refresh()
-        stdscr.getkey()
+        key = stdscr.getch()
 
 wrapper(main)
