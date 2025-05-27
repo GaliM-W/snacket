beae01f693877e5d38a3832575cc287379c96dcc
camcamcamcamcam <eboysix@hotmail.co.nz>
2025-05-27 14:39:51 +1200

    Second snake for testing controlled with IJKL

--- Main.py
+++ Main.py
@@ -16,20 +16,29 @@ def main(stdscr):
             [[0, 0, Part.TAIL], [0, 1, Part.BODY], [1, 1, Part.LUMP], [2, 1, Part.HEAD]]
         )
     )
+    board.add_snake(
+        Snake(
+            [[5, 5, Part.TAIL], [5, 4, Part.BODY], [4, 4, Part.LUMP], [4, 3, Part.HEAD]]
+        )
+    )
     board.add_food(5, 5)
     board.add_wall(9, 9)
-    key = 0
-    while key != ord("Q"):
-        direction = "asdw".find(chr(key))
-        if 0 <= direction <= 3:
-            for snake in board.snakes:
-                snake.facing = Direction(direction)
+    key1 = 0
+    key2 = 0
+    while key1 != ord("Q"):
+        direction1 = "asdw".find(chr(key1))
+        direction2 = "jkli".find(chr(key2))
+        if 0 <= direction1 <= 3 and 0 <= direction2 <= 3:
+            board.snakes[0].facing = Direction(direction1)
+            if len(board.snakes) > 1:
+                board.snakes[1].facing = Direction(direction2)
 
         stdscr.clear()
         board.tick()
         stdscr.addstr(str(board))
         stdscr.refresh()
-        key = stdscr.getch()
+        key1 = stdscr.getch()
+        key2 = stdscr.getch()
 
 
 wrapper(main)
